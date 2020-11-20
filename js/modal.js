363562a4b3e48e12573e44910e3a5a82558df6d2
Várdai Enikő <vardai.eniko@gmail.com>
2020-11-20 12:11:42 +0100

    window

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,33 @@
 'use strict';
 
-const button = document.querySelector(".button__modal");
 const body = document.querySelector('body');
+const button = document.querySelector(".button__modal");
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const icon = document.querySelector("i");
+const okey = document.querySelector(".modal__okey");
+const cancel = document.querySelector(".modal__cancel");
 
-button.addEventListener('click', function () {
+const modalWindow = () => {
     modal.setAttribute("class", "modal animation");
-    body.setAttribute("style", "background-color: rgba(128, 128, 128, 0.5)");
     modal.setAttribute("style", "display: block");
+    overlay.setAttribute("style", "display: block");
     button.setAttribute("tabindex", "-1");
     modal.focus();
-})
+}
 
 const backToMain = (wichButton) => {
     console.log(wichButton);
     modal.setAttribute("class", "modal animation2");
     setTimeout(function () {
-        body.setAttribute("style", "background-color: none");
-        modal.setAttribute("style", "display: none");       
+        overlay.setAttribute("style", "display: none");
+        modal.setAttribute("style", "display: none");
     }, 900);
 }
 
-const icon = document.querySelector("i");
+button.addEventListener('click', modalWindow);
 icon.addEventListener('click', backToMain);
-
-const cancel = document.querySelector(".modal__cancel");
-cancel.addEventListener('click', ()=>backToMain('false'));
-
-const okey = document.querySelector(".modal__okey");
-okey.addEventListener('click', ()=>backToMain('true'));
-
-/* window.addEventListener('click', function () {
-        body.setAttribute("style", "background-color: red");
-        modal.setAttribute("style", "display: none");
-
-}); */
+cancel.addEventListener('click', () => backToMain('false'));
+okey.addEventListener('click', () => backToMain('true'));
+overlay.addEventListener('click', backToMain);
 
